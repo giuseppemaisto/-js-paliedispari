9e561a920c7c13a5fea5ed3d8d3464db341fefc0
giuseppemaisto <gmaisto98@gmail.com>
2022-12-05 15:19:48 +0100

    inserimento pari dispari e numero

---
+++ pari e dispari/js/script.js
@@ -0,0 +1,5 @@
+
+//1 - Dare la possibilità di scegliere pari o dispari 
+let pariDispari = prompt("scegli pari o dispari");
+//2 - inserire numero da 1 a 5 
+let numUtente = parseInt(prompt("inserisci numero da 1 a 5"))

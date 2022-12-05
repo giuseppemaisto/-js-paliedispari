8a541e9d7c1e18d60ba6e4ea97c4a0027622b96c
giuseppemaisto <gmaisto98@gmail.com>
2022-12-05 14:42:23 +0100

    inserita funzione reverse

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -6,7 +6,10 @@ console.log(parola_utente)
 function palindroma(parola){
 // 2.1 - usare la funzione split per generare un array partendo dalla parola inserita 7
 let parolaArray = parola.split();
-
+console.log(parolaArray)
+//2.2 - usare la funzione reverse per invertire l'ordine degli elementi 
+let parolaReverse = parolaArray.reverse();
+console.log(parolaReverse);
 
 
 }

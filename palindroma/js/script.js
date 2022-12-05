ecc9829feb259f062b4c43f4a20ac48ea9a59871
giuseppemaisto <gmaisto98@gmail.com>
2022-12-05 14:38:17 +0100

    aggiunto inserimento parola e funzione split

---
+++ palindroma/js/script.js
@@ -0,0 +1,12 @@
+// 1 - Permettere di inserire una parola 
+let parola_utente = prompt("inserisci parola")
+console.log(parola_utente)
+
+//2 - Creare una funzione che stampi se la parola è palindroma o meno 
+function palindroma(parola){
+// 2.1 - usare la funzione split per generare un array partendo dalla parola inserita 7
+let parolaArray = parola.split();
+
+
+
+}

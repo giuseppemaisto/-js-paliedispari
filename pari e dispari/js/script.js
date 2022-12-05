46343b02adec3a4828962aee22c4e885b33877a0
giuseppemaisto <gmaisto98@gmail.com>
2022-12-05 15:41:20 +0100

    definita funzione numero casuale pc

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,5 +1,12 @@
 
 //1 - Dare la possibilità di scegliere pari o dispari 
 let pariDispari = prompt("scegli pari o dispari");
+console.log(pariDispari)
 //2 - inserire numero da 1 a 5 
 let numUtente = parseInt(prompt("inserisci numero da 1 a 5"))
+console.log(numUtente)
+//3 - Generiamo un numero casuale da 1 a 5 con una funzione
+function numeroCasuale(){
+    let NumPc = Math.floor(Math.random() * 5)+1;
+    return NumPc;
+}

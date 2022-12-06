a46373c31cec78bc9a345a81eb568b819a5e32db
giuseppemaisto <gmaisto98@gmail.com>
2022-12-06 10:09:51 +0100

    esercizio corretto: TERMINATO

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -6,30 +6,30 @@ console.log(pariDispari)
 let numUtente = parseInt(prompt("inserisci numero da 1 a 5"))
 console.log("il tuo numero è "+numUtente)
 //3 - Generiamo un numero casuale da 1 a 5 con una funzione
-function numeroCasuale(){
-    let NumPc = Math.floor(Math.random() * 5)+1;
-    return NumPc;
+function numeroCasuale(min, max){
+   
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let numero = numeroCasuale();
-console.log("il numero del pc è " + numero);
+let numPc = numeroCasuale(1,5);
+console.log("il numero del pc è " + numPc);
 //4 - sommiamo i due numeri 
 // 4.1 - definiamo variabile somma 
-let somma = numUtente + numero;
+let somma = numUtente + numPc;
 console.log("la somma è "+ somma);
 //5 - stabiliamo e la somma è pari o dispari tramite una funzione
    // 5.1 - Se il risultato dell'operatore resto di somma da 0 allora è apri 
    // ALTRIMENTI è dispari 
 
 
-   function SommaPariDispari(numero){
-    let num = "";
-    if(numero % 2 === 0){
-        num = "pari";
+   function SommaPariDispari(somma){
+    
+    if(somma % 2 == 0){
+      return  "pari";
     }
-    else{
-        num = "dispari";
-    }
-    return num;
+    
+        return "dispari";
+    
+ 
    }
    let esito = SommaPariDispari(somma);
    
@@ -37,11 +37,8 @@ console.log("la somma è "+ somma);
    //6 - stampa risultato 
 // 6.1 - Se utente ha scelto pari e la somma è pari allora ha vinto 
  //ALTRIMENTI ha vinto il computer 
- if(pariDispari === "pari"){
-    console.log("hai vinto")
- }else if(pariDispari === "dispari"){
-    console.log("hai perso")
- }
- else{
-    console.log("non hai inserito pari o dispari")
+ if(pariDispari == esito){
+    console.log("hai vinto");
+ }else {
+    console.log("hai perso");
  }
